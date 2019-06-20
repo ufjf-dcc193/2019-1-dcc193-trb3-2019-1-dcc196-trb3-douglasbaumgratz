aa031360947eaf894b9e47c98e0770510fcd4363
douglasBaumgratz <douglas.baumgratz@gmail.com>
2019-06-19 21:42:42 -0300

    Inserindo index.html e controller para atende lo

--- src/main/java/br/ufjf/dcc193/trabalho03/Controllers/HomeController.java
+++ src/main/java/br/ufjf/dcc193/trabalho03/Controllers/HomeController.java
@@ -1,6 +1,8 @@
 package br.ufjf.dcc193.trabalho03.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  *
@@ -9,5 +11,15 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class HomeController {
 
+    /**
+     *
+     * @return
+     */
+    @RequestMapping({ "/", "/index.html" })
+    public ModelAndView index() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("index");        
+        return mv;
+    }
 
 }
