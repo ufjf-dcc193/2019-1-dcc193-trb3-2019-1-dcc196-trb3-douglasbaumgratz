c79e4819f94a6c3b0b9298d67d77e74551ae219d
douglasBaumgratz <douglas.baumgratz@gmail.com>
2019-06-19 21:03:05 -0300

    Inserindo dependências e mudando  demo para Main

---
+++ src/main/java/br/ufjf/dcc193/trabalho03/Controllers/HomeController.java
@@ -0,0 +1,13 @@
+package br.ufjf.dcc193.trabalho03.Controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ *
+ * @authors douglas
+ */
+@Controller
+public class HomeController {
+
+
+}
