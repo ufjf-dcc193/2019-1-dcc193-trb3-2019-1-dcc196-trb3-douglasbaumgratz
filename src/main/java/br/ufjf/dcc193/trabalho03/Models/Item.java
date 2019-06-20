008acce5e9f360720b0f49ee2a3cf4fbeb33bc50
douglasBaumgratz <douglas.baumgratz@gmail.com>
2019-06-19 21:07:25 -0300

    Criando classe Item

---
+++ src/main/java/br/ufjf/dcc193/trabalho03/Models/Item.java
@@ -0,0 +1,76 @@
+package br.ufjf.dcc193.trabalho03.Models;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ * Item
+ */
+@Entity
+@Table(name = "item")
+public class Item {    
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;    
+    private String titulo;
+    @OneToMany(fetch = FetchType.EAGER)
+    private List<Anotacao> anotacao;
+    @OneToMany(fetch = FetchType.EAGER)
+    private List<Etiqueta> etiqueta;
+    @OneToMany(fetch = FetchType.EAGER)
+    private List<Vinculo> vinculo;
+
+    public Item(Long id, String titulo) {
+        this.id = id;
+        this.titulo = titulo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public List<Anotacao> getAnotacao() {
+        return anotacao;
+    }
+
+    public void setAnotacao(List<Anotacao> anotacao) {
+        this.anotacao = anotacao;
+    }
+
+    public List<Etiqueta> getEtiqueta() {
+        return etiqueta;
+    }
+
+    public void setEtiqueta(List<Etiqueta> etiqueta) {
+        this.etiqueta = etiqueta;
+    }
+
+    public List<Vinculo> getVinculo() {
+        return vinculo;
+    }
+
+    public void setVinculo(List<Vinculo> vinculo) {
+        this.vinculo = vinculo;
+    }
+
+    
+}
